dd8f5b84b5bb150387cf964c5a072a019ca1d338
testuserMNL <Rblxmull@gmail.com>
2022-06-13 16:07:32 +0700

    commit

--- Bebek/bbk.js
+++ Bebek/bbk.js
@@ -4,6 +4,9 @@ char1.src = "Images/char1.png";
 var char2=new Image();
 char2.src = "Images/char2.png";
 
+var Title=new Image();
+Title.src = "Images/judul.png";
+
 var back=new Image();
 back.src = "Images/bg.jpg";
 
@@ -25,19 +28,37 @@ canvas.height = canvas.scrollHeight;
 var cW = canvas.width;
 var cH = canvas.height;
 
+var bgX = 0, start = false;
+function splash() {
+ctx.clearRect(0,0,cW,cH);
+ctx.drawImage(back, bgX -= 2, 0);
+if (bgX==- 1598) {
+    bgX = 0;
+}
 
-// function splash() {
+ctx.font = "bold 20px arial";
+ctx.fillText("Click to Start", 300, 400);
 
-// }
+ctx.drawImage(Title, 120, 100);
+}
 
-main();
+var inSplash = setInterval(splash, 30);
+
+document.addEventListener('click', function (event) {
+    if (start==false) {
+        start = true;
+        clearInterval(inSplash);
+        main();
+    }
+});
+
     function main() {
         var imagechange = false;
 
         function BG () {
             this.x = 0;
             this.render=function() {
-                ctx.drawImage (back, this.x--, 0);
+                ctx.drawImage (back, this.x--, this.i= 0);
 
                 if (this.x == -1599) {
                     this.x = 0;
@@ -64,12 +85,11 @@ main();
         var character = new Character();
 
         var Pole=[];   
-        
         addPole();
         function addPole () {
             var x=800, y = 0, w = 50, h = 300;
-            var mix = Math.floor(Math.random()*200)
-            Pole.push({"x":x, "y":y-mix, "w":w, "h":h})
+            var mix = Math.floor(Math.random()*200);
+            Pole.push({"x":x, "y":y-mix, "w":w, "h":h});
         }
 
         var count = 0;
@@ -88,9 +108,9 @@ main();
             ctx.fillText("Score : "+score , 150, 340);
         }
 
-        var score = 0, addnum=true;
+        var score = 0, addNum=true;
         function addscore () {
-            score++
+            score++;
         }
 
         function hit () {
@@ -98,10 +118,10 @@ main();
                 var p=Pole[i];
                 if((character.x + character.w > p.x && character.y < p.y+p.h && character.x<p.x+p.w)||(character.x+character.w > p.x && character.y+character.h > p.y+p.h+220 && character.x < p.x + p.w)) {
                     end();
-                }else if (p.x+p.w<character) {
-                    if (addnum) {
+                }else if (p.x + p .w < character.x) {
+                    if (addNum) {
                         addscore();
-                        addnum = false;
+                        addNum = false;
                     }
                 }
             }
@@ -112,15 +132,15 @@ main();
 
         function renderPole() {
             for (var i=0; i<Pole.length; i++) {
-                var p=Pole[i];
+                var p = Pole[i];
                 ctx.drawImage(pole, p.x--, p.y);
-                ctx.drawImage(pole, p.x--, p.y+p.h+ 220);
-                if (p.x+p.w<0) {
+                ctx.drawImage(pole, p.x--, p.y+p.h + 220);
+                if (p.x+p.w < 0) {
                     Pole.splice(i,1);
-                    addnum = true;
+                    addNum = true;
                 }
             }
-            count++
+            count++;
             if (count==150){
                 addPole();
                 count = 0;
